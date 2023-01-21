e5032c787e845b13770cf15d0e5e9006a04ce789
Alexander Weichart <alexanderweichart@fastmail.com>
2023-01-21 16:07:51 +0100

    basic chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {
+	MainContainer,
+	ChatContainer,
+	MessageList,
+	Message,
+	MessageInput,
+} from "@chatscope/chat-ui-kit-react";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => (
+	<div style={{ position: "relative", height: "100%" }}>
+		<MainContainer>
+			<ChatContainer>
+				<MessageList>
+					<Message
+						model={{
+							message: "Hello my friend",
+							sentTime: "just now",
+							sender: "Joe",
+							direction: "outgoing",
+							position: "single",
+						}}
+					/>
+				</MessageList>
+				<MessageInput attachButton={false} placeholder="Type message here" />
+			</ChatContainer>
+		</MainContainer>
+	</div>
+);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
-
-export default App
+export default App;
